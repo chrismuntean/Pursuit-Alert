a1ca9bc6b8ea923f9b1945104e070fc728edc5be
Chris Muntean <chrismunt123@gmail.com>
2024-04-06 16:20:55 -0500

    Added analysis homepage dataframe

--- pages/Analysis.py
+++ pages/Analysis.py
@@ -1,6 +1,53 @@
 import os
+import pandas as pd
+
 import json
 import streamlit as st
+
+def display_dataframe():
+
+    # check if the all_plates.json file exists
+    if os.path.exists("logs/perm/all_plates.json"):
+
+        # get the all_plates.json file
+        with open("logs/perm/all_plates.json", "r") as file:
+            all_plates = json.load(file)
+
+        # Create a list and append the data to it
+        data = []
+
+        for plate, detections in all_plates.items():
+            data.append({
+                "analyze": "/Analysis?plate=" + plate,
+                "plate": plate,
+                "detection_count": str(len(detections)), # Convert to str to align left
+                "first_seen": detections[0]["date"] + " " + detections[0]["time"],
+                "last_seen": detections[-1]["date"] + " " + detections[-1]["time"],
+                "risk": "High" if len(detections) > 5 else "Low"
+            })
+
+        # Create a pandas DataFrame from the list
+        df = pd.DataFrame(data)
+
+        # Display the DataFrame using streamlit
+        all_plates_dataframe.dataframe(
+            df,
+            column_config={
+                "analyze": st.column_config.LinkColumn("Analyze", display_text = "View media"),
+                "plate": "Plate",
+                "detection_count": "Sightings",
+                "first_seen": "First Seen",
+                "last_seen": "Last Seen",
+                "risk": "Risk"
+            },
+
+            hide_index=True,
+            use_container_width=True
+        )
+
+    else:
+        # if the all_plates.json file does not exist, display an error
+        st.error("No plates detected yet")
 
 # Check if a variable was passed in the URL query string
 plate = st.query_params.get("plate")
@@ -47,5 +94,10 @@ if plate != None:
 
 
 else:
-    st.write("No plate specified, displaying default dataframe.")
-    # Display the default dataframe or any other default information
+    # No plate specified so display the default dataframe to allow the user to choose one
+    st.header("Analysis", divider = 'gray')
+
+    # create an empty dataframe object
+    all_plates_dataframe = st.empty()
+
+    display_dataframe()
